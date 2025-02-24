43a63d5537034a834f77715d4d0dc049537f4769
frostie :3 <156554853+frostiefops@users.noreply.github.com>
2025-02-24 17:38:40 +1300

    Add files via upload

---
+++ gpio.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef CFG_GPIO_H
+#define CFG_GPIO_H
+
+#include <gpio.h>
+
+/* Pad configuration was generated automatically using intelp2m 1.1-e95fd3e041 */
+static const struct pad_config gpio_table[] = {
+};
+
+#endif /* CFG_GPIO_H */
